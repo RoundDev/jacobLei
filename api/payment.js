0e3c71e2f9c6819b0ed5711ecc5d085f8a18a2d4
josh <parton.joshua@gmail.com>
2019-03-05 18:59:47 -0500

    updated paymentjs to create customer object

--- api/payment.js
+++ api/payment.js
@@ -21,37 +21,62 @@ square.sendSquarePayment = function(req,res,next){
 		let amtTotal = (amtInDollars * 0.0375) + amtInDollars;
 		let amt = parseInt(amtTotal, 10);
 		console.log('This is amt' + ' ' +  amt);
+		let given_name = req.body.first_name;
+		let family_name = req.body.last_name;
+		let email_address = req.body.email_address;
+		let phone_number = req.body.phone_number;
 		
 		let location_id = process.env.SQUARE_LOCATION_TEST;//"CBASEKMX2G17bvMoK22CqyjodIYgAQ";
 		let access_token = process.env.SQUARE_TOKEN_TEST;//"sandbox-sq0atb-z_RHpdCXPfJTFaf1itVRjQ";
 		
-		unirest.post('https://connect.squareup.com/v2/locations/' + location_id + '/transactions')
-		.headers({
-			'Accept': 'application/json',
-			'Content-Type': 'application/json',
-			'Authorization': 'Bearer ' + access_token,
-		})
-		.send({
-			'card_nonce':nonce /*NONCE field from form*/,
-			'amount_money': {
-				'amount': amt /*amount field from form*/,
-				'currency':'USD'
-			},
-			'idempotency_key':uuidv1()
-		})
-		.end(function(response){
-		  console.log('Respons paymnent' + '' + response);
-      // if(response.statusCode === 200) {
-      //   // alert('Payment send')
-      //   console.log('Success')
-      // }else if(response.statusCode !== 200) {
-      //   // alert('Payment Fail' + res.json(error))
-      //   console.log('Fail')
-      // }
-		  // console.log(response);
-			res.json(response)
+		let customer = new Promise((resolve) => {
+			unirest.post('https://connect.squareup.com/v2/customers')
+			.headers({
+				'Accept': 'application/json',
+				'Content-Type': 'application/json',
+				'Authorization': 'Bearer ' + access_token,
+			})
+			.send({
+				given_name,
+				family_name,
+				email_address,
+				phone_number
+			})
+			.end(function(response){
+				resolve(response);
+			})
+		});
 
-		})
+		customer.then(data => {
+			unirest.post('https://connect.squareup.com/v2/locations/' + location_id + '/transactions')
+			.headers({
+				'Accept': 'application/json',
+				'Content-Type': 'application/json',
+				'Authorization': 'Bearer ' + access_token,
+			})
+			.send({
+				'card_nonce':nonce /*NONCE field from form*/,
+				'amount_money': {
+					'amount': amt /*amount field from form*/,
+					'currency':'USD'
+				},
+				'idempotency_key':uuidv1(),
+				'customer_id' : data.id
+			})
+			.end(function(response){
+			console.log('Respons paymnent' + '' + response);
+		// if(response.statusCode === 200) {
+		//   // alert('Payment send')
+		//   console.log('Success')
+		// }else if(response.statusCode !== 200) {
+		//   // alert('Payment Fail' + res.json(error))
+		//   console.log('Fail')
+		// }
+			// console.log(response);
+				res.json(response)
+
+			})
+		});
 	}
 	catch(error){
 		console.log("\x1b[31m%s\x1b[0m",`ERROR: ${error}`);
