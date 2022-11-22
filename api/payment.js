8b07537282459fc3f6b1e43ee10f44442fdea4ea
Michael Kaminski <mkaminski69@gmail.com>
2022-11-21 20:39:12 -0500

    Square SDK
    - Updated dist folder

--- api/payment.js
+++ api/payment.js
@@ -9,37 +9,42 @@ const { Client, Environment, PaymentsApi, CustomersApi } = require('square');
 
 async function sendSquarePayment(req, res, next) {
 
-  let amount = (parseInt(req.body.amountToPay, 10) * 100);
+  try {
 
-  const client = new Client({
-    accessToken: process.env.SQUARE_TOKEN_PROD ,
-    environment: Environment['Production']
-  });
+    let amount = (parseInt(req.body.amountToPay, 10) * 100);
 
-  const customer = {
-    givenName: req.body.first_name,
-    familyName: req.body.last_name,
-    emailAddress: req.body.email_address,
-    phoneNumber: req.body.phone_number
+    const client = new Client({
+      accessToken: process.env.SQUARE_TOKEN_PROD,
+      environment: Environment['Production']
+    });
+
+    const customer = {
+      givenName: req.body.first_name,
+      familyName: req.body.last_name,
+      emailAddress: req.body.email_address,
+      phoneNumber: req.body.phone_number
+    }
+
+    const payment = {
+      token: req.body.token,
+      amountMoney: {
+        amount,
+        currency: 'USD',
+      },
+      appFeeMoney: {
+        amount: Math.round((amount * 0.0375)),
+        currency: 'USD'
+      },
+    }
+    // create customer
+    let createdCustomer = await createCustomer(client, customer);
+    payment.customerId = createdCustomer.id;
+    let createdPayment = await createPayment(client, payment);
+    // console.log('Created payment Payload' + '\n', createdPayment);
+    return res.json(createdPayment.status);
+  } catch (err) {
+    return JSON.stringify(err);
   }
-
-  const payment = {
-    token: req.body.token,
-    amountMoney: {
-      amount,
-      currency: 'USD',
-    },
-    appFeeMoney: {
-      amount: Math.round((amount * 0.0375)),
-      currency: 'USD'
-    },
-  }
-  // create customer
-  let createdCustomer = await createCustomer(client, customer);
-  payment.customerId = createdCustomer.id;
-  let createdPayment = await createPayment(client, payment);
-  // console.log('Created payment Payload' + '\n', createdPayment);
-  return res.json(createdPayment.status);
 }
 
 async function createPayment(client, payment){
