486aa361c58f97e1baffa3725476530051477baa
Michael Kaminski <mkaminski69@gmail.com>
2023-05-25 22:27:06 -0400

    - Updated build

--- dist/assets/js/hideTable.js
+++
@@ -1,7 +0,0 @@
-// window.onload = function(){
-//   document.getElementById("desktop").style.display = "none";
-//   document.getElementById("inspection").style.display = "none";
-//   document.getElementById("accounting").style.display = "none";
-//   document.getElementById("communication").style.display = "none";
-// };
-
